47a4ca76a0943ae6730832c0fcb34681439bffce
knawara <blueeyedhush@gmail.com>
2017-12-19 13:16:11 +0100

    introducing start.sh file which sets required environment variables

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM node:9.3.0-alpine
 COPY build/ /webapp/build/
 COPY node_modules/ /webapp/node_modules/
-RUN cd /webapp/ && node build/node_modules/server
+COPY start.sh /webapp/
+WORKDIR /webapp/
+RUN ./start.sh
---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# this file is copied inside docker image
+# expects to be run from /webapp/ directory within container
+
+export NODE_ENV=production
+export SERVER_ROOT=/webapp/build/node_modules/server/
+export CLIENT_ROOT=/webapp/build/node_modules/client/
+node build/node_modules/server
